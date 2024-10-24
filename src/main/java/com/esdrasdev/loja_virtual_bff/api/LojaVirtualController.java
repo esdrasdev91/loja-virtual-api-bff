6474844a1471cc61e5bca94d8862c0eaeb3e42fc
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-24 16:12:32 -0300

    Create new class controller

---
+++ src/main/java/com/esdrasdev/loja_virtual_bff/api/LojaVirtualController.java
@@ -0,0 +1,13 @@
+package com.esdrasdev.loja_virtual_bff.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/loja-virtual")
+@RequiredArgsConstructor
+@Tag(name = "loja-virtual-api")
+public class LojaVirtualController {
+
+
+}
