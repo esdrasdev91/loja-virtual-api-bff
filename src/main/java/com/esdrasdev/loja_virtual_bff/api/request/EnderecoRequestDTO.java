0c220f7ec64bef184f73b39e39e1599d523bcabc
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-25 09:59:46 -0300

    Create new enderecoRequestDTO

---
+++ src/main/java/com/esdrasdev/loja_virtual_bff/api/request/EnderecoRequestDTO.java
@@ -0,0 +1,17 @@
+package com.esdrasdev.loja_virtual_bff.api.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EnderecoRequestDTO {
+
+    private String rua;
+    private Long numero;
+    private String bairro;
+    private String complemento;
+    private String cidade;
+    private String cep;
+
+}
