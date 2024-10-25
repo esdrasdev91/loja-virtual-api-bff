a4cf9b0516f0fb90ba75a412f51a53233be716b0
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-25 09:56:15 -0300

    Create LojaVirtualConverter

---
+++ src/main/java/com/esdrasdev/loja_virtual_bff/api/converter/LojaVirtualConverter.java
@@ -0,0 +1,34 @@
+package com.esdrasdev.loja_virtual_bff.api.converter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LojaVirtualConverter {
+
+    public CartaoRequestDTO paraCartaRequestoDTO(String nome, CompraRequestDTO compraRequestDTO, BigDecimal valor,
+                                                 EnderecoResponseDTO enderecoResponseDTO) {
+
+        return CartaoRequestDTO.builder()
+                .nome(nome)
+                .enderecoRequestDTO(paraEnderecoRequestDTO(enderecoResponseDTO))
+                .numeroCartao(compraRequestDTO.getNumeroCartao())
+                .ano(compraRequestDTO.getAno())
+                .mes(compraRequestDTO.getMes())
+                .cvv(compraRequestDTO.getAno())
+                .valor(valor)
+                .build();
+
+    }
+
+    private EnderecoRequestDTO paraEnderecoRequestDTO(EnderecoResponseDTO enderecoResponseDTO) {
+        return EnderecoRequestDTO.builder()
+                .bairro(enderecoResponseDTO.getBairro())
+                .cep(enderecoResponseDTO.getCep())
+                .cidade(enderecoResponseDTO.getCidade())
+                .complemento(enderecoResponseDTO.getComplemento())
+                .numero(enderecoResponseDTO.getNumero())
+                .rua(enderecoResponseDTO.getRua())
+                .build();
+    }
+
+}
