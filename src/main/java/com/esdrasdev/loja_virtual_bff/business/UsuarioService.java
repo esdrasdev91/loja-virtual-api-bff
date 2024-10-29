ed8843603832a45edda5e050daa71a54a3e36ac7
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-29 10:02:52 -0300

    Create class UsuarioService

---
+++ src/main/java/com/esdrasdev/loja_virtual_bff/business/UsuarioService.java
@@ -0,0 +1,26 @@
+package com.esdrasdev.loja_virtual_bff.business;
+
+import com.esdrasdev.loja_virtual_bff.api.request.UsuarioRequestDTO;
+import com.esdrasdev.loja_virtual_bff.api.response.UsuarioResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioClient usuarioClient;
+
+    public UsuarioResponseDTO cadastrarUsuario(UsuarioRequestDTO usuarioRequestDTO) {
+        return usuarioClient.gravaDadosUsuario(usuarioRequestDTO);
+    }
+
+    public UsuarioResponseDTO buscaUsuarioPorEmail(String email) {
+        return usuarioClient.buscaUsuarioPorEmail(email);
+    }
+
+    public void deletaUsuarioPorEmail(String email) {
+        usuarioClient.deletaDadosUsuario(email);
+    }
+
+}
