de6f7f12251dbe6510a144f72a48e8a7030ec45e
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-29 10:00:45 -0300

    Create clÃass ProdutosService

---
+++ src/main/java/com/esdrasdev/loja_virtual_bff/business/ProdutosService.java
@@ -0,0 +1,23 @@
+package com.esdrasdev.loja_virtual_bff.business;
+
+import com.esdrasdev.loja_virtual_bff.api.response.ProductsDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProdutosService {
+
+    private final ProdutosClient client;
+
+    public List<ProductsDTO> buscaTodosProdutos() {
+        return client.buscaTodosProdutos();
+    }
+
+    public ProductsDTO buscaProdutosPorNome(String nome) {
+        return client.buscaProdutoPorNome(nome);
+    }
+
+}
