6b17c5cff61161561a145a925c4dce7e2a20f6fb
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-31 10:25:09 -0300

    add annotation class

--- src/main/java/com/esdrasdev/loja_virtual_bff/infrastructure/client/usuarioclient/UsuarioClient.java
+++ src/main/java/com/esdrasdev/loja_virtual_bff/infrastructure/client/usuarioclient/UsuarioClient.java
@@ -2,8 +2,10 @@ package com.esdrasdev.loja_virtual_bff.infrastructure.client.usuarioclient;
 
 import com.esdrasdev.loja_virtual_bff.api.request.UsuarioRequestDTO;
 import com.esdrasdev.loja_virtual_bff.api.response.UsuarioResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
+@FeignClient(value = "usuario-api", url = "localhost:8282")
 public interface UsuarioClient {
 
     @PostMapping("/user")
